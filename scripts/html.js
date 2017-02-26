2d987e8cecc4895f605ebff5ee890bba2e7e3a71
mstssk <mstssk+github@gmail.com>
2017-02-26 17:35:30 +0900

    wip

--- scripts/html.js
+++ scripts/html.js
@@ -1,11 +1,11 @@
 "use strict";
 
 const fs = require("fs");
-const yaml = require("yaml");
+const yaml = require("js-yaml");
 const mustache = require("mustache");
 const config = require("../config");
 
-const template = fs.readFileSync("src/index.html").toString();
-const data = yaml.eval(fs.readFileSync("src/data.yml").toString());
+const template = fs.readFileSync("src/index.html", 'utf8');
+const data = yaml.safeLoad(fs.readFileSync("src/data.yml", 'utf8'));
 const rendered = mustache.render(template, data);
 fs.writeFileSync(`${config.output}/index.html`, rendered, { encoding: "utf-8" });
