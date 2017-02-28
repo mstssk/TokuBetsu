fa6525bb534391b35c25d4bf38957a07d20bf7f3
mstssk <mstssk+github@gmail.com>
2017-03-01 01:04:46 +0900

    chore: ignore imgages dir when clean

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,2 +1,15 @@
 const config = require("../config");
-require("rimraf").sync(config.output);
+const rimraf = require("rimraf");
+const glob = require('glob');
+glob(`${config.output}/**/*`,
+    { ignore: `${config.output}/images/**` },
+    (err, matches) => {
+        logError(err);
+        matches.forEach(p => rimraf(p, e => logError(e)));
+    });
+
+function logError(err) {
+    if (err) {
+        console.error(err);
+    }
+}
