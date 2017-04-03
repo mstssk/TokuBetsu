8e26a1b2d9bd008ea24d39aee928ed02b390c6f7
mstssk <mstssk+github@gmail.com>
2017-04-03 23:11:47 +0900

    copy

---
+++ scripts/copy.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const glob = require("glob");
+const config = require("../config");
+
+const files = glob.sync('src/+(images|js)/*');
+files.forEach(file => {
+    const path = file.slice('src/'.length);
+    const output = `${config.output}/assets/${path}`;
+    const outputDir = output.substr(0, output.lastIndexOf('/'));
+    // console.log(file, path, output, outputDir);
+    require('mkpath').sync(outputDir);
+    fs.createReadStream(file).pipe(fs.createWriteStream(output));
+});
