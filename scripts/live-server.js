ae0c35385fa8e3e84e33d4cb30abe84a19b5f055
mstssk <mstssk+github@gmail.com>
2017-04-03 23:44:01 +0900

    eyecatch

--- scripts/live-server.js
+++ scripts/live-server.js
@@ -5,6 +5,7 @@ const packageJson = require("../package.json");
 const mountPath = `/${packageJson.name}/`;
 const output = `${config.output}/`;
 liveServer.start({
+    wait: 3000,
     mount: [[mountPath, output]],
     open: [mountPath],
     root: '__dummy__',
