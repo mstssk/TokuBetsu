676e50935c43461f272af53d56f186d630d83f96
mstssk <mstssk+github@gmail.com>
2017-07-28 03:00:30 +0900

    abbr_name

--- scripts/html.js
+++ scripts/html.js
@@ -10,6 +10,7 @@ const data = yaml.safeLoad(fs.readFileSync("src/data.yml", 'utf8'));
 data.books.forEach((book, i) => {
     const even = i % 2 === 0;
     book.__swap_right__ = even ? 'swap-right' : '';
+    book.abbr_name = book.abbr_name || book.name;
 });
 const rendered = mustache.render(template, data);
 fs.writeFileSync(`${config.output}/index.html`, rendered, { encoding: "utf-8" });
